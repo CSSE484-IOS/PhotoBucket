f67d0a379d8b14f9f8221dc1bf17bcd244768053
Yizhi Feng <fengy2@rose-hulman.edu>
2018-04-13 16:02:01 -0400

    created storyboard, removed template view controllers and constructed core data object

---
+++ PhotoBucket/PhotoBucketDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PhotoBucketDetailViewController.swift
+//  PhotoBucket
+//
+//  Created by FengYizhi on 2018/4/13.
+//  Copyright © 2018年 FengYizhi. All rights reserved.
+//
+
+import UIKit
+
+class PhotoBucketDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
